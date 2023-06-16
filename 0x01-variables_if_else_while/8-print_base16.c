7bf7db95144300ff6ce5d1158336d9eeaab438de
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-06-16 08:51:08 -0700

    Add 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descreption: Prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	n = 0;
+	while (n <= 16)
+	{
+		if (n < 10)
+			putchar('0' + n);
+		else
+			putchar('a' + n - 10);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
