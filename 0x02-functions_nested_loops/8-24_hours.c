dd96b2ff040ff0fee654c5621e2487acb7dbb23f
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-06-20 12:25:32 -0700

    Add 8-24_hours.c & Update main.h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer.
+ */
+void jack_bauer(void)
+{
+	int n1, n2;
+
+	n1 = 0;
+	n2 = 0;
+	while (n1 < 24)
+	{
+		while (n2 < 60)
+		{
+			_putchar('0' + n1 / 10);
+			_putchar('0' + n1 % 10);
+			_putchar(':');
+			_putchar('0' + n2 / 10);
+			_putchar('0' + n2 % 10);
+			_putchar('\n');
+			n2++;
+		}
+		n1++;
+		n2 = n1 + 1;
+	}
+	_putchar('\n');
+}
