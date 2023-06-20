2666ce0e47cd3b0e2ddadb1aa28b9989309f0087
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-06-20 11:30:12 -0700

    Fix: 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,14 @@
  */
 int print_last_digit(int n)
 {
-	int a = _abs(n);
-
-	_putchar('0' + (a % 10));
-	return (a % 10);
+	if (n >= 0)
+	{
+		_putchar('0' + (n % 10));
+		return (n % 10);
+	}
+	else
+	{
+		_putchar('0' + (-n % 10));
+		return (-n % 10);
+	}
 }
