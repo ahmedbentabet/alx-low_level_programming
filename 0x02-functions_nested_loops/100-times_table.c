b9315e23834bb4c028cc0177f246877b8038882c
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-06-20 17:42:51 -0700

    Fix: 100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,14 +2,22 @@
 
 /**
  * print_times_table - Prints the n times table, starting with 0.
- * @n: the number of the times table.
+ * @n: the number of the times table
  */
 void print_times_table(int n)
 {
-	int n1, n2;
-
 	if (n > 15 || n < 0)
 		return;
+	print_table(n);
+}
+
+/**
+ * print_table - Prints the times table up to the given number.
+ * @n: the number of the times table
+ */
+void print_table(int n)
+{
+	int n1, n2;
 
 	for (n1 = 0; n1 < n + 1; n1++)
 	{
@@ -17,28 +25,8 @@ void print_times_table(int n)
 		{
 			int a = n1 * n2;
 
-			if (n2 == 0)
-			{
-				_putchar('0');
-			}
-			else if (a < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + a);
-			}
-			else if (a < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + a / 10);
-				_putchar('0' + a % 10);
-			}
-			else
-			{
-				_putchar('0' + a / 100);
-				_putchar('0' + ((a / 10) % 10));
-				_putchar('0' + a % 10);
-			}
+			print_number(a);
+
 			if (n2 != n)
 			{
 				_putchar(',');
@@ -46,5 +34,31 @@ void print_times_table(int n)
 			}
 		}
 		_putchar('\n');
+	}
+}
+
+/**
+ * print_number - Prints a number in the times table format.
+ * @num: the number to be printed
+ */
+void print_number(int num)
+{
+	if (num < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('0' + num);
+	}
+	else if (num < 100)
+	{
+		_putchar(' ');
+		_putchar('0' + num / 10);
+		_putchar('0' + num % 10);
+	}
+	else
+	{
+		_putchar('0' + num / 100);
+		_putchar('0' + ((num / 10) % 10));
+		_putchar('0' + num % 10);
 	}
 }
