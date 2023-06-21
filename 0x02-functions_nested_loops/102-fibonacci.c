2be046243aa03bbb7b006feadaa2b040a5d84975
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-06-20 18:15:03 -0700

    Add 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - Prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count = 0;
+	long int previous, current, next;
+
+	previous = current = 1;
+	while (count < 50)
+	{
+		if (count == 49)
+		{
+			printf("%llu\n", current);
+		}
+		else
+		{
+			printf("%llu, ", current);
+		}
+		next = previous + current;
+		previous = current;
+		current = next;
+		count++;
+	}
+	return 0;
+}
+
