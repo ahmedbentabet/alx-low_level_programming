7b1c89bf04270fdc5501b0676a33488f354d5316
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-06-26 15:38:35 -0700

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string from source to destination
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: A pointer to the destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
