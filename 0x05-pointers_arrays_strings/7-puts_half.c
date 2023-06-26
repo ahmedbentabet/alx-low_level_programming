ba58c81fe0c87f379afa48b9517c372b0c9a89f8
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-06-26 12:54:26 -0700

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: The input string
+ */
+void puts_half(char *str)
+{
+	int length;
+	int start;
+	int i;
+
+	length = _strlen(str);
+	start = (length + 1) / 2;
+	i = start;
+	while (i < length)
+	{
+		putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - Calculates the length of a string
+ * @s: The string to calculate the length of
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int  length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
