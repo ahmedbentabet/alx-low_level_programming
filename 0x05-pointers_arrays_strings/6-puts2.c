099215f38a83e806b0d679201b9b4a86a92c7079
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-06-26 11:46:53 -0700

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: The string to be printed
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		str++;
+	}
+	_putchar('\n');
+}
