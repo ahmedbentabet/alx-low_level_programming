69d05460c8ce50cf393210711572cbf2244652e0
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-06-26 15:18:14 -0700

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - Prints elements of an array
+ * @a: The input array
+ * @n: The number of elements in the array
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+}
