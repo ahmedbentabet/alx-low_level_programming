ee8cb828b51a44b90775bf2f79445f4d0d12564d
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-06-29 16:38:52 -0700

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: The difference between the ASCII values of the first non-matching
+ * characters in s1 and s2, or 0 if the strings are equal.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
