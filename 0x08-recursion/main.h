e1da9feff11fa4881f8c7ecd95b82bc2f748a358
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-07-05 16:19:02 -0700

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int my_sqrt(int a, int b);
 int is_prime_number(int n);
+int my_prime(int n, int divisor);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
