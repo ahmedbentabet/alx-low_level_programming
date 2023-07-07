f078f5a2906d846d23e96608e3c39e122e5f75d5
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-07-07 15:42:41 -0700

    Fix: 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,10 +13,8 @@ int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
-	if (argc <= 1)
-	{
+	if (argc == 1)
 		printf("0\n");
-	}
 
 	for (i = 1; i < argc; i++)
 	{
@@ -28,7 +26,6 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-
 		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
