efb9366b1f623a83402543e7f6a9be776736fcda
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-07-07 12:46:38 -0700

    Fix: betty in 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-int main (int argc, char *argv[])
+/**
+ * main - Prints its name, followed by a new line.
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
