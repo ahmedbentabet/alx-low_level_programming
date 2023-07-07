163924503965fccd0566550213f7c7042ba70871
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-07-07 13:49:53 -0700

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Prints the number of arguments passed into it.
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
