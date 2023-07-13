081dac7f9fc167249c80a2d79b6f6104822d8f68
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-07-13 15:59:27 -0700

    Update 100-realloc.c & main.h

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,8 +7,7 @@
  * @old_size: Size of the previously allocated memory block
  * @new_size: New size of the memory block to be allocated
  *
- * Return: Pointer to the newly allocated memory block,
- * or NULL on failure or if new_size == old_size
+ * Return: Pointer to the newly allocated memory block, or NULL on failure or if new_size == old_size
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
@@ -36,33 +35,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_ptr == NULL)
 		return (NULL);
 
-	if (old_size < new_size)
-		_memcpy(new_ptr, (char *)ptr, old_size);
-
-	if (old_size > new_size)
-		_memcpy(new_ptr, (char *)ptr, new_size);
+	for (i = 0; i < old_size && i < new_size; i++)
+		new_ptr[i] = ((char *)ptr)[i];
 
 	free(ptr);
 	return (new_ptr);
-}
-
-/**
- * _memcpy - copies n bytes from memory area src to memory area dest
- * @dest: pointer to the destination memory area
- * @src: pointer to the source memory area
- * @n: number of bytes to copy
- *
- * Return: pointer to the destination memory area (dest)
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i = 0;
-
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-
-	return (dest);
 }
