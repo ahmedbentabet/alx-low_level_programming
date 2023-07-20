c14d559c3ae3de9522e34193515800f65a5d50f9
ahmedbentabet <61090548+AhmedBENTABET@users.noreply.github.com>
2023-07-20 16:24:04 -0700

    Create 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Calculates the sum of its variable number of arguments.
+ * @n: The number of arguments passed to the function.
+ * @...: A variable number of integers to be summed.
+ *
+ * Return: The sum of the integers. If n is 0, returns 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum;
+
+	if (n == 0)
+		return (0);
+
+	va_list args;
+	va_start(args, n);
+
+	sum = 0;
+	for(i = 0; i < n; i++)
+	{
+		int x = va_arg(args, int);
+		sum += x;
+	}
+
+	va_end(args);
+	return (sum);
+}
