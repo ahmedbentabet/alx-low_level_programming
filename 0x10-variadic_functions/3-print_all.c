39932e8e4bb03757d1e38fafd601dce303e7ce1b
ahmedbentabet <bentabet841@gmail.com>
2023-08-17 17:44:00 -0700

    Fix task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,41 +4,47 @@
  * print_all - Prints anything based on the format.
  * @format: A list of types of arguments passed to the function.
  */
-void print_all(const char *const format, ...)
+
+void print_all(const char * const format, ...)
 {
+	int i;
+	char *s;
+	char *sep;
 	va_list args;
-	unsigned int i = 0;
-	char *str;
-	char format_char;
+
+	i = 0;
+	sep = "";
 	va_start(args, format);
-	while (format && format[i])
-	{
-		format_char = format[i];
-		switch (format_char)
+
+	if (format)
+		while (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					printf("(nil)");
-				else
-					printf("%s", str);
-				break;
-			default:
-				break;
+			switch (format[i])
+			{
+				case 'i':
+					printf("%s%d", sep, va_arg(args, int));
+					break;
+				case 'c':
+					printf("%s%c", sep, va_arg(args, int));
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					if (!s)
+						s = "(nil)";
+					printf("%s%s", sep, s);
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(args, double));
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
 		}
-		if (format[i + 1] != '\0' && (format_char == 'c' || format_char == 'i' || format_char == 'f' || format_char == 's'))
-			printf(",");
-		i++;
-	}
-	va_end(args);
+
 	printf("\n");
+	va_end(args);
 }
+
