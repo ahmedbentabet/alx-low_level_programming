37d98ed278ef96df7d42b112bb57b8f62cc6c014
ahmedbentabet <bentabet841@gmail.com>
2023-08-17 17:32:09 -0700

    Fix task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,19 +4,16 @@
  * print_all - Prints anything based on the format.
  * @format: A list of types of arguments passed to the function.
  */
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
 	char *str;
 	char format_char;
-
 	va_start(args, format);
-
 	while (format && format[i])
 	{
 		format_char = format[i];
-
 		switch (format_char)
 		{
 			case 'c':
@@ -38,14 +35,10 @@ void print_all(const char * const format, ...)
 			default:
 				break;
 		}
-
 		if (format[i + 1] != '\0' && (format_char == 'c' || format_char == 'i' || format_char == 'f' || format_char == 's'))
-			printf(", ");
-
+			printf(",");
 		i++;
 	}
-
 	va_end(args);
 	printf("\n");
 }
-
