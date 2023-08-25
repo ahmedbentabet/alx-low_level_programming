802750e7a87ea31f17d58060638adbf00adc45fc
ahmedbentabet <bentabet841@gmail.com>
2023-08-24 19:58:58 -0700

    Add task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+ * pre_main - prints a string before the main function is executed
+ */
 void __attribute__((constructor)) pre_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
