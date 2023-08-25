e4d264f0003f6a4aae272dbb7876011815268e02
ahmedbentabet <bentabet841@gmail.com>
2023-08-24 19:57:52 -0700

    Add task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,6 @@
 
 void __attribute__((constructor)) pre_main()
 {
-	printf("You're beat! and yet, you must allow,\n"
-		"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
