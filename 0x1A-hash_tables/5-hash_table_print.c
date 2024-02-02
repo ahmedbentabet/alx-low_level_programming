8a1b666f9b05bf054d8272335ed83289ece78aac
Ahmed Bentabet <bentabet841@gmail.com>
2024-02-02 21:57:35 +0000

    Add task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current_node;
+	int print_once = 0;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			current_node = ht->array[i];
+			while (current_node != NULL)
+			{
+				if (print_once != 0)
+					printf(", ");
+
+				printf("'%s' : '%s'", current_node->key, current_node->value);
+				current_node = current_node->next;
+				if (print_once == 0) print_once = 1;
+			}
+		}
+	}
+	printf("}\n");
+}
