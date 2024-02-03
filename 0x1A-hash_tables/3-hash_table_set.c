af339d700c548ef65f04fc02d6f8bf126658c33e
Ahmed Bentabet <bentabet841@gmail.com>
2024-02-03 11:06:26 +0000

    Update task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -69,8 +69,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/*key not found, so lets add it*/
 	new_node = create_node(key, value);
 	if (new_node == NULL)
+	{
 		free(new_node);
 		return (0);
+	}
 
 	if (ht->array[index] == NULL)
 		ht->array[index] = new_node;
