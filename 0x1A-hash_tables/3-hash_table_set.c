c9f8bc70b346a2700850120d6b6f41452ea4cee8
Ahmed Bentabet <bentabet841@gmail.com>
2024-02-03 10:58:48 +0000

    Update task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,35 @@
 #include "hash_tables.h"
+
+/**
+ * create_node - creates a new node
+ *
+ * @key: the key for the new node
+ * @value: the value for the new node
+ *
+ * Return: a pointer to the new node, or NULL on failure
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
+}
+
 /**
  * hash_table_set - adds an element to the hash table
  *
@@ -12,10 +43,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *current_node = ht->array[index];
-	hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	hash_node_t *new_node;
 
-	if (new_node == NULL || ht == NULL || key == NULL || key[0] == '\0')
+	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (0);
+
 	while (current_node)
 	{
 		if (strcmp(current_node->key, key) == 0)
@@ -24,28 +56,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			current_node->value = strdup(value);
 			if (current_node->value == NULL)
 				return (0);
-			return (1);  /*Key found and updated*/
+			return (1); /* Key found and updated */
 		}
 		current_node = current_node->next;
 	}
-	/*Key not found, we neet to add it*/
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	if (new_node->key == NULL || new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node->value);
-		free(new_node);
+
+	/*key not found, so lets add it*/
+	new_node = create_node(key, value);
+	if (new_node == NULL)
 		return (0);
-	}
-	new_node->next = NULL;   /*Set the next pointer of the new node to NULL*/
-	if (ht->array[index] == NULL) /*insert the new node*/
+
+	if (ht->array[index] == NULL)
 		ht->array[index] = new_node;
 	else
-	{	/*If there's collision*/
-		current_node = ht->array[index];
+	{
 		new_node->next = current_node;
 		ht->array[index] = new_node;
 	}
+
 	return (1);
 }
